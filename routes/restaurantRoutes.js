9a85b8ce6cd10dc09ee266286366b98db047c73e
MabohlaleNkuna <mabohlalenkuna@gmail.com>
2025-01-07 13:05:18 +0200

    added restaurant upd and del functionality

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,12 +1,28 @@
 const express = require('express');
-const { getAllRestaurants, createRestaurant } = require('../controllers/restaurantController');
+const { 
+  getAllRestaurants, 
+  createRestaurant, 
+  updateRestaurant, 
+  deleteRestaurant, 
+  deleteAllRestaurants // Optional
+} = require('../controllers/restaurantController');
 const multer = require('multer');
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
+// Routes for all restaurants
 router.route('/')
   .get(getAllRestaurants)
   .post(upload.single('image'), createRestaurant);
+
+// Routes for updating and deleting restaurants by ID
+router.route('/:id')
+  .put(upload.single('image'), updateRestaurant)
+  .delete(deleteRestaurant); // Delete a single restaurant by ID
+
+// Route to delete all restaurants (Optional)
+router.route('/deleteAll')
+  .delete(deleteAllRestaurants);
 
 module.exports = router;
