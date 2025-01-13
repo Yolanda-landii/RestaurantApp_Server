2109502f3f5b7b2d58fc584c9391725fdd963194
MabohlaleNkuna <111710433+MabohlaleNkuna@users.noreply.github.com>
2025-01-13 11:18:45 +0200

    increased storage limit

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+app.use(express.json({ limit: '10mb' })); 
+app.use(express.urlencoded({ limit: '10mb', extended: true })); 
 
 // Middleware
 app.use(express.json());
